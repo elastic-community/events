bc9154eac017082299100482db65f566d6803485
Manuel Alba <malba@mmae.cl>
2023-02-24 21:47:16 -0300

    change name

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,4 @@
 import Navbar from "./navbar";
-
 export default function Layout({ children }) {
   return (
     <>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,4 @@
-
 import Link from 'next/link'
-
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
