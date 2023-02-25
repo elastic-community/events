a8a6f2a761aea0fcdd3dc44fdf7850fe91159edf
Manuel Alba <malba@mmae.cl>
2023-02-24 21:52:09 -0300

    fix

--- pages/api/other-events.js
+++ pages/api/other-events.js
@@ -1,18 +1,13 @@
 import apm from 'elastic-apm-node'
 
 
-import { prisma } from '../../db'
 
 export default async function handler(req, res) {
 
     var span = apm.startSpan('call DB')
 
 
-    const regions = await prisma.regions.findMany({
-        include: {
-          airports: true, // Return all fields
-        },
-      })
+    const regions = []
     res.status(200).json(regions)
 
     if (span) span.end()
