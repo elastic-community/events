583325ed67ede7b59679e6fbb70d1904e873bfd9
Manuel Alba <malba@mmae.cl>
2023-02-24 22:31:29 -0300

    fix rum

--- rum.js
+++ rum.js
@@ -2,15 +2,15 @@ import { init as initApm } from '@elastic/apm-rum'
 var apm = initApm({
 
   // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
-  serviceName: 'JsConf Chile -Frontend',
+  serviceName: 'events-assistant',
 
   // Set custom APM Server URL (default: http://localhost:8200)
-  serverUrl: '',
+  serverUrl: 'https://c9f68453570140e1bc57b00e42fa6db3.apm.us-central1.gcp.cloud.es.io:443',
 
   // Set the service version (required for source map feature)
   serviceVersion: '1.0',
 
   // Set the service environment
-  environment: 'development'
+  environment: 'production'
 })
 export default apm;
