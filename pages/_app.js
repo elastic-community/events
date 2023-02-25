fffd69f1a71d07b3df608afe2cd856141a2c8357
Manuel Alba <malba@mmae.cl>
2023-02-24 22:46:41 -0300

    update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ export default function MyApp({ Component, pageProps }) {
     return (
         <>     
          <Head>
-            <title>JS Conf Example</title>
+            <title>Elastic community - events-assistants</title>
             <link rel="icon" href="/favicon.ico" />
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"/> 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
