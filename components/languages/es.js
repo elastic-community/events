ef01713c9bd49f07f8c35b14986771f2403b2147
Manuel Alba <malba@mmae.cl>
2023-03-02 02:06:12 -0300

    ready to prod

--- components/languages/es.js
+++ components/languages/es.js
@@ -14,7 +14,6 @@ const lang = {
     acceptMarketing:'¿Está interesado en suscribirse a nuestras actualizaciones de marketing?',
     comercialTeam:'¿Le gustaría estar conectado con nuestro equipo de ventas para cualquier solicitud comercial?',
     comments:'¡Otros comentarios!',
-
     localGroupField_1: 'Sí',
     localGroupField_2: 'No',
     localGroupField_3: 'Tal vez, ¡Cuéntame más!',
@@ -35,11 +34,11 @@ const lang = {
     familyContributeProgram_3: 'Soy un colaborador anterior',
     familyContributeProgram_4: 'Estoy contribuyendo al ciclo actual',
     familyContributeProgram_5: 'Soy un usuario avanzado',        
-
     acceptMarketing_1: 'Sí',
     acceptMarketing_2: 'No',
     comercialTeam_1: 'Sí',
     comercialTeam_2: 'No',
+    success: '¡Gracias por registrarse!',
 
 }
 export default lang;
