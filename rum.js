1a4942708886f86b72e0a333752fc3a059e8aa35
Manuel Alba <malba@mmae.cl>
2023-03-23 01:20:26 -0300

    Update rum.js

--- rum.js
+++ rum.js
@@ -1,12 +1,11 @@
 import { init as initApm } from '@elastic/apm-rum'
-console.log("ENV",process.env.OTEL_EXPORTER_OTLP_ENDPOINT)
 var apm = initApm({
 
   // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
   serviceName: 'events-assistant-frontend',
 
   // Set custom APM Server URL (default: http://localhost:8200)
-  serverUrl: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
+  serverUrl:"https://55bb8f51cb3e4d20918063edafc37244.apm.us-east-2.aws.elastic-cloud.com:443",
   // Set the service version (required for source map feature)
   serviceVersion: '1.0',
 
